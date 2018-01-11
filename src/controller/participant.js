1e78d7a70f82709ab3304dfa5363433e5c7eb657
José Fonseca <jose.fonseca@mindera.com>
2018-01-11 22:37:33 +0000

    fix(user-access): Add/Remove participant-user access

--- src/controller/participant.js
+++ src/controller/participant.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import Participant from '../model/participant'
+import User from '../model/user'
 import isLoggedIn from '../context/isLoggedIn'
-import {DatabaseORM} from '../database';
+import {DB} from '../database';
 
 
 let router = express.Router();
@@ -12,7 +13,7 @@ let fields = ['name', 'gender', 'country', 'city'];
  */
 router.post('/participant', isLoggedIn, function (req, res) {
 
-    DatabaseORM.transaction((tx) => {
+    DB.transaction((tx) => {
         let participant = new Participant();
         // filter body fields
         fields.forEach((f) => {
@@ -24,7 +25,7 @@ router.post('/participant', isLoggedIn, function (req, res) {
 
 
         return participant.save(null, {transacting: tx})
-            // grant ADMIN permisions to creating user
+        // grant ADMIN permisions to creating user
             .then((participant) => req.user.participants().attach({
                     id_participant: participant.id,
                     role: 'ADMIN'
@@ -74,12 +75,117 @@ router.get('/participants', isLoggedIn, function (req, res) {
 router.get('/participant/:id_participant', isLoggedIn, function (req, res) {
     req.user.connectedParticipant(req.params.id_participant, ['ADMIN', 'EDITOR', 'TERAPEUT', 'VIEWER'])
         .then((participant) => {
-        if (!participant) {
-            res.status(404).send({});
-        } else {
-            res.send(participant.toJSON({omitPivot: true}));
-        }
-    });
+            if (!participant) {
+                res.status(404).send({});
+            } else {
+                res.send(participant.toJSON({omitPivot: true}));
+            }
+        });
+});
+
+
+/**
+ * Returns the list of users who have direct access to a participant
+ * has access to
+ */
+router.get('/participant/:id_participant/users', isLoggedIn, function (req, res, next) {
+    req.user.connectedParticipant(req.params.id_participant)
+        .then((participant) => {
+            if (!participant) {
+                res.status(404).send({});
+            } else {
+                participant.users().fetch({columns: ['id', 'email', 'name', 'profile_photo', 'role']}).then((users) => {
+                    res.send(users.toJSON({omitPivot: true}));
+                });
+            }
+        })
+        .catch(next);
+});
+
+/**
+ * Returns the list of users who have direct access to a participant
+ * has access to
+ */
+router.post('/participant/:id_participant/users', isLoggedIn, function (req, res, next) {
+    req.user.connectedParticipant(req.params.id_participant, ['ADMIN'])
+        .then((participant) => {
+            if (!participant) {
+                res.status(403).send({status: 'ERROR', message: 'PERMISSION_DENIED'});
+            } else {
+                if (req.body.email == req.user.get('email')) {
+                    res.status(409).send({status: 'ERROR', message: 'CANNOT_SET_OWN_ACCESS_LEVELS'});
+                } else if (req.body.id) {
+                    return DB('user_participant')
+                        .update({role: req.body.role})
+                        .where({
+                            id_participant: participant.get('id'),
+                            id_user: req.body.id
+                        })
+                        .then(() => {
+                            res.send({status: 'SUCCESS', message: 'PARTICIPANT_USER_ROLE_UPDATED'});
+                        })
+                } else if (req.body.email) {
+                    return User.where({email: req.body.email}).fetch().then((user) => {
+                        if (user) {
+                            // user already in our system, so we can just attach the participant
+                            return user.participants().attach({
+                                id_participant: participant.get('id'),
+                                role: 'VIEWER'
+                            }).then(() => {
+                                res.send({status: 'SUCCESS', message: 'USER_ADDED_TO_PARTICIPANT'});
+                            });
+                        } else {
+                            // user does not exist which means we need to create a new user and attach the participant to it
+                            user = new User();
+                            user.set('email', req.body.email);
+                            return user.save().then(() => {
+                                user.participants().attach({
+                                    id_participant: participant.get('id'),
+                                    role: 'VIEWER'
+                                }).then(() => {
+                                    res.send({status: 'SUCCESS', message: 'USER_ADDED_TO_PARTICIPANT'});
+                                });
+                            })
+                        }
+                    });
+                } else {
+                    res.status(400).send({status: 'ERROR', message: 'USER_DETAILS_MISSING'});
+                }
+            }
+        })
+        .catch(next);
+});
+
+
+
+/**
+ * Returns the list of users who have direct access to a participant
+ * has access to
+ */
+router.delete('/participant/:id_participant/users', isLoggedIn, function (req, res, next) {
+    req.user.connectedParticipant(req.params.id_participant, ['ADMIN'])
+        .then((participant) => {
+            if (!participant) {
+                res.status(403).send({status: 'ERROR', message: 'PERMISSION_DENIED'});
+            } else {
+                if (req.body.email == req.user.get('email')) {
+                    res.status(409).send({status: 'ERROR', message: 'CANNOT_SET_OWN_ACCESS_LEVELS'});
+                } else if (req.body.id) {
+                    return DB('user_participant')
+                        .where({
+                            id_participant: participant.get('id'),
+                            id_user: req.body.id
+                        })
+                        .delete()
+                        .then(() => {
+                            res.send({status: 'SUCCESS', message: 'PARTICIPANT_USER_ROLE_UPDATED'});
+                        })
+                } else {
+                    res.status(400).send({status: 'ERROR', message: 'USER_DETAILS_MISSING'});
+                }
+            }
+        })
+        .catch(next);
 });
 
 
@@ -89,24 +195,24 @@ router.get('/participant/:id_participant', isLoggedIn, function (req, res) {
 router.post('/participant/:id_participant', isLoggedIn, function (req, res) {
     req.user.connectedParticipant(req.params.id_participant, ['ADMIN', 'EDITOR', 'TERAPEUT'])
         .then((participant) => {
-        if (participant) {
-            fields.forEach((f) => {
-                participant.set(f, req.body[f]);
-            });
+            if (participant) {
+                fields.forEach((f) => {
+                    participant.set(f, req.body[f]);
+                });
 
-            // convert date to object
-            participant.set('dob', new Date(req.body.dob));
+                // convert date to object
+                participant.set('dob', new Date(req.body.dob));
 
-            participant.save().then(() => {
-                res.send({status: 'SUCCESS', message: 'PARTICIPANT_DETAILS_SAVED'});
-            }).catch(() => {
-                res.status(500).send({status: 'ERROR', message: 'ERROR_UPDATING_PARTICIPANT_DETAILS'});
-            })
+                participant.save().then(() => {
+                    res.send({status: 'SUCCESS', message: 'PARTICIPANT_DETAILS_SAVED'});
+                }).catch(() => {
+                    res.status(500).send({status: 'ERROR', message: 'ERROR_UPDATING_PARTICIPANT_DETAILS'});
+                })
 
-        } else {
-            res.status(404);
-        }
-    });
+            } else {
+                res.status(404);
+            }
+        });
 });
 
 
